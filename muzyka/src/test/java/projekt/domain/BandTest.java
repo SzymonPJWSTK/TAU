53c3eb5bf729cca7a66d1cc63cc54f7e0a2dfe0f
SzymonPJWSTK <s15754@pjwstk.edu.pl>
2019-03-05 10:21:36 +0100

    Dodanie dao i domain oraz testy czy istnieja klasy

---
+++ muzyka/src/test/java/projekt/domain/BandTest.java
@@ -0,0 +1,22 @@
+package projekt.domain;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class BandTest{
+    @Test
+    public void createobjectTest(){
+        Band b = new Band();
+        assertNotNull(b);
+    }
+
+    @Test
+    public void gettersAndSettersTest(){
+        Band b = new Band();
+        b.setId(2);
+        assertEquals(2,b.getId());
+    }
+}
